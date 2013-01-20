8f7f0f70a41a7ccbe3642ca25de0a20e9c08390d
Devin Lindsay <devinalindsay@gmail.com>
2013-01-20 17:18:06 -0500

    Adding Initial Files

---
+++ www/js/global.js
@@ -0,0 +1,8 @@
+// JavaScript Document
+
+$(document).bind("mobileinit", function(){
+	$.mobile.defaultPageTransition = 'none';
+	$.mobile.defaultDialogTransition = 'none';
+});
+
+//gets rid of page transitions because they do not work well on Android devices
